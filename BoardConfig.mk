855553aa4034b997a8efe496787b821c2d4a89cc
Hassan Sardar <hassansardar08@live.com>
2018-01-05 12:22:13 +0400

    j7y17lte: Use prebuilt dtb for now
    -Until the kernel and dt stuff is improved.
    
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,10 +25,7 @@ TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7870
 TARGET_KERNEL_CONFIG := lineage-j7y17lte_defconfig
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
-BOARD_KERNEL_SEPARATED_DT := true
-TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/dtb.img
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432
